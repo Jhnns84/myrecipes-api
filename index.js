e33fe7190a95ddebe41014730dd7e73086f538d7
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-06-15 18:44:02 +0200

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // imports CORS module and defines allowed domains
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://jhnns84.github.io/myrecipes-app/', 'https://jm-myrecipes-api.herokuapp.com/', 'https://myrecipes-app-jhnns84.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://jhnns84.github.io/myrecipes-app/', 'https://myrecipes-app-jhnns84.netlify.app/', 'https://jm-myrecipes-api.herokuapp.com/'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);

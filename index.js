6fbf296cf05445ddbc92d2dfc3e4851b98da965f
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-06-16 10:22:06 +0200

    changed cors implementation

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ app.use(express.json());
 // imports CORS module and defines allowed domains
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myrecipes-app-jhnns84.netlify.app/'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn´t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // imports auth-file
 require('./auth')(app);
